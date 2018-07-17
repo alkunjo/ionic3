a0f1e4e8ed3b954f01b3e7d2a28e652b3fa977a1
Alan Kuncoro Raharjo <alankuncororaharjo@gmail.com>
2018-07-17 23:54:23 +0700

    Learning ionic basic components

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, ErrorHandler } from "@angular/core";
+import { IonicApp, IonicModule, IonicErrorHandler } from "ionic-angular";
+import { MyApp } from "./app.component";
+
+import { HelloIonicPage } from "../pages/hello-ionic/hello-ionic";
+import { ItemDetailsPage } from "../pages/item-details/item-details";
+import { ListPage } from "../pages/list/list";
+import { ActionSheets } from "../pages/action-sheets/action-sheets";
+
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HelloIonicPage,
+    ItemDetailsPage,
+    ListPage,
+    ActionSheets
+  ],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HelloIonicPage,
+    ItemDetailsPage,
+    ListPage,
+    ActionSheets
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
+  ]
+})
+export class AppModule {}
---
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "page-hello-ionic",
+  templateUrl: "hello-ionic.html"
+})
+export class HelloIonicPage {
+  constructor() {}
+}
